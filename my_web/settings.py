cdf2028b7b75b313341776d909044fe6c6a5d512
rotonda <rotondwamulelu@gmail.com>
2020-07-23 14:10:28 +0200

    add sqlite3db

--- my_web/settings.py
+++ my_web/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'my_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
-        'USER': 'myuser',
-        'PASSWORD': 'mypass',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
